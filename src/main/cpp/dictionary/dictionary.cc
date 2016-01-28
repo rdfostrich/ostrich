0460cd03b981835b7471086267b4cc8691ccd009
Miel Vander Sande <miel.vandersande@ugent.be>
2016-01-28 16:30:58 +0100

    Make dictionary compile

--- src/main/cpp/dictionary/dictionary.cc
+++ src/main/cpp/dictionary/dictionary.cc
@@ -1,28 +1,30 @@
-#include <HDTManager.hpp>
-#include <HDTVocabulary.hpp>
+#include <iostream>
+#include <string>
+
 #include <Dictionary.hpp>
-#include <FourSectionDictionary.hpp>
+#include <HDTSpecification.hpp>
+#include <HDTVocabulary.hpp>
 #include <PlainDictionary.hpp>
+#include <Triples.hpp>
 
 using namespace std;
 using namespace hdt;
 
-
 class DictionaryManager : ModifiableDictionary {
 
-    Dictionary* hdtDict; // Dictionary from HDT file
-    ModifiableDictionary* patchDict; // Additional dictionary
+  Dictionary *hdtDict;             // Dictionary from HDT file
+  ModifiableDictionary *patchDict; // Additional dictionary
 
-    const unsigned int bitmask;
+  const unsigned int bitmask;
 
 public:
-  DictionaryManager(Dictionary* hdtDict, HDTSpecification &spec): bitmask( 0x80000000 ), hdtDict(hdtDict) {
+  DictionaryManager(Dictionary *hdtDict, HDTSpecification &spec)
+      : hdtDict(hdtDict), bitmask(0x80000000) {
     // Create additional dictionary
-  	patchDict = new PlainDictionary(spec);
+    patchDict = new PlainDictionary(spec);
   };
 
-  std::string idToString(unsigned int id, TripleComponentRole position)
-  {
+  std::string idToString(unsigned int id, TripleComponentRole position) {
     // Check whether id is from HDT or not (MSB is not set)
     if (id & bitmask) {
       return hdtDict->idToString(id, position);
@@ -31,25 +33,23 @@ public:
     return patchDict->idToString(id, position);
   }
 
-  unsigned int stringToId(std::string &key, TripleComponentRole position)
-  {
+  unsigned int stringToId(std::string &key, TripleComponentRole position) {
     // First ask HDT
-    try
-    {
+    try {
       return hdtDict->stringToId(key, position);
-    } catch( exception e ){} // ID is not in there
+    } catch (exception e) {
+    } // ID is not in there
 
     // Set MSB to 1
     return bitmask | patchDict->stringToId(key, position);
   }
 
-  unsigned int insert(std::string &str, TripleComponentRole position)
-  {
+  unsigned int insert(std::string &str, TripleComponentRole position) {
     // First ask HDT
-    try
-    {
+    try {
       return hdtDict->stringToId(str, position);
-    } catch( exception e ) {} // ID is not in there
+    } catch (exception e) {
+    } // ID is not in there
 
     return patchDict->insert(str, position);
   }
