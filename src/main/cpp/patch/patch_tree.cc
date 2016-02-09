7b7dfb8f4a43c10b71b32a3865fcb48a10537b29
Ruben Taelman <ruben.taelman@ugent.be>
2016-02-09 10:25:16 +0100

    Add patch tree key comparator

--- src/main/cpp/patch/patch_tree.cc
+++ src/main/cpp/patch/patch_tree.cc
@@ -7,24 +7,43 @@ using namespace std;
 using namespace kyotocabinet;
 
 PatchTree::PatchTree(string file_name) {
-    // open the database
+    // Open the database
     if (!db.open(file_name, HashDB::OWRITER | HashDB::OCREATE)) {
         cerr << "open error: " << db.error().name() << endl;
     }
+
+    // Set the triple comparator
+    class ComparatorImpl : public Comparator {
+        int32_t compare(const char* akbuf, size_t aksiz, const char* bkbuf, size_t bksiz) {
+            PatchTreeKey* element1 = (PatchTreeKey*) akbuf;
+            PatchTreeKey* element2 = (PatchTreeKey*) bkbuf;
+            int comp_subject = element1->subject.compare(element2->subject);
+            if(!comp_subject) {
+                int comp_predicate = element1->predicate.compare(element2->predicate);
+                if(!comp_predicate) {
+                    return element1->object.compare(element2->object);
+                }
+                return comp_predicate;
+            }
+            return comp_subject;
+        };
+    };
+    ComparatorImpl comparator = ComparatorImpl();
+    db.tune_comparator(&comparator);
 };
 
 PatchTree::~PatchTree() {
-    // close the database
+    // Close the database
     if (!db.close()) {
         cerr << "close error: " << db.error().name() << endl;
     }
 }
 
-void PatchTree::append(PatchElement* patch) {
-    PatchElement* current = patch;
+void PatchTree::append(PatchElements* patch) {
+    PatchElements* current = patch;
     while(current != NULL) {
         // TODO: write into tree
-        cout << "appending... " << current->triple.subject << endl;
+        cout << "appending... " << current->patchElement.triple.subject << endl;
         current = current->next;
     }
 }
--- src/main/cpp/patch/patch_tree.h
+++ src/main/cpp/patch/patch_tree.h
@@ -7,6 +7,7 @@
 using namespace std;
 using namespace kyotocabinet;
 
+// S, P, O holder
 typedef struct Triple {
     string subject;
     string predicate;
@@ -15,29 +16,37 @@ typedef struct Triple {
             subject(subject), predicate(predicate), object(object) {}
 } Triple; // TODO: use dictionary
 
+// A triple annotated with addition or deletion
 typedef struct PatchElement {
     Triple triple;
     bool addition;
-    PatchElement* next;
     PatchElement(Triple triple, bool addition) : triple(triple), addition(addition) {}
 } PatchElement;
 
+// A linked list of PatchElements
+typedef struct PatchElements {
+    PatchElement patchElement;
+    PatchElements* next;
+    PatchElements(PatchElement patchElement) : patchElement(patchElement) {}
+} PatchElements;
+
+// A key in the PatchTree is a triple
 typedef Triple PatchTreeKey;
 
+// A value in the PatchTree is a linked list of patch_id->patch_position
 typedef struct PatchTreeValue {
     int patch_id;
     int patch_position;
     PatchTreeValue* next;
 } PatchTreeValue;
 
-class PatchTree
-{
+class PatchTree {
 private:
     TreeDB db;
 public:
     PatchTree(string file_name);
     ~PatchTree();
-    void append(PatchElement* patch);
+    void append(PatchElements* patch);
 };
 
 #endif //TPFPATCH_STORE_PATCH_TREE_H
