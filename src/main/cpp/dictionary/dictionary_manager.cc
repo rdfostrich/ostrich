fd64a9245aae0c46ca74cc593f9fbe722cae2e0f
Miel Vander Sande <miel.vandersande@ugent.be>
2016-06-07 16:20:10 +0200

    Added dictionary manager

--- src/main/cpp/dictionary/dictionary_manager.cc
+++ src/main/cpp/dictionary/dictionary_manager.cc
@@ -11,6 +11,9 @@
 using namespace std;
 using namespace hdt;
 
+DictionaryManager::DictionaryManager(Dictionary *hdtDict, ModifiableDictionary *patchDict)
+    : hdtDict(hdtDict), patchDict(patchDict), bitmask(2147483648) {};
+
 DictionaryManager::DictionaryManager(Dictionary *hdtDict)
     : hdtDict(hdtDict), bitmask(2147483648) {
   // Create additional dictionary
@@ -30,6 +33,8 @@ DictionaryManager::~DictionaryManager() {
 
 std::string DictionaryManager::idToString(unsigned int id,
                                           TripleComponentRole position) {
+  if (id == 0) return "";
+
   // Check whether id is from HDT or not (MSB is not set)
   if (!(id & bitmask)) {
     return hdtDict->idToString(id, position);
@@ -38,8 +43,12 @@ std::string DictionaryManager::idToString(unsigned int id,
   return patchDict->idToString(id - bitmask, position);
 }
 
+//TODO: make sure 0 is reserved for variables
 unsigned int DictionaryManager::stringToId(std::string &str,
                                            TripleComponentRole position) {
+  // if string is empty, it's a variable, and thus, a zero
+  if (str.empty()) return 0;
+
   // First ask HDT
   unsigned int id;
   try {
@@ -48,25 +57,117 @@ unsigned int DictionaryManager::stringToId(std::string &str,
       return id;
   } catch (exception e) {
   } // ID is not in there
-  // cout << "Snapshot dictionary does not have " << str << endl;
+  //cout << "Snapshot dictionary does not have " << str << endl;
 
   // Set MSB to 1
   id = patchDict->stringToId(str, position);
+  //cout << "Patch dictionary gived id  " << id << endl;
   return bitmask | id;
 }
 
 unsigned int DictionaryManager::insert(std::string &str,
                                        TripleComponentRole position) {
+
+  if (str.empty()) return 0;
+
   // First ask HDT
   unsigned int id;
   try {
     id = hdtDict->stringToId(str, position);
+    cout << "In HDT, " << str << " has id " << id << endl;
     if (id > 0)
       return id;
   } catch (exception e) {
   } // ID is not in there
 
+  id = bitmask | patchDict->insert(str, position);
+  cout << "Patch dictionary gives id  " << id << endl;
   // cout << "Snapshot dictionary does not have " << str << endl;
 
-  return bitmask | patchDict->insert(str, position);
+  return id;
 }
+
+size_t DictionaryManager::getNumberOfElements() {
+  return hdtDict->getNumberOfElements() + patchDict->getNumberOfElements();
+}
+
+uint64_t DictionaryManager::size() {
+  return hdtDict->size() + patchDict->size();
+}
+
+unsigned int DictionaryManager::getNsubjects() {
+  return hdtDict->getNsubjects() + patchDict->getNsubjects();
+}
+
+unsigned int DictionaryManager::getNpredicates() {
+  return hdtDict->getNpredicates() + patchDict->getNpredicates();
+}
+
+unsigned int DictionaryManager::getNobjects() {
+  return hdtDict->getNobjects() + patchDict->getNobjects();
+}
+
+unsigned int DictionaryManager::getNshared() {
+  return hdtDict->getNshared() + patchDict->getNshared();
+}
+
+unsigned int DictionaryManager::getMaxID() {
+  return patchDict->getMaxID();
+}
+
+unsigned int DictionaryManager::getMaxSubjectID() {
+  return patchDict->getMaxSubjectID();
+}
+
+unsigned int DictionaryManager::getMaxPredicateID() {
+  return patchDict->getMaxPredicateID();
+}
+
+unsigned int DictionaryManager::getMaxObjectID() {
+  return patchDict->getMaxObjectID();
+}
+
+void DictionaryManager::populateHeader(Header &header, string rootNode) {}
+
+void DictionaryManager::save(std::ostream &output, ControlInformation &ci, ProgressListener *listener) {
+  patchDict->save(output, ci, listener);
+}
+
+void DictionaryManager::load(std::istream &input, ControlInformation &ci, ProgressListener *listener) {
+  patchDict->load(input, ci, listener);
+}
+
+size_t DictionaryManager::load(unsigned char *ptr, unsigned char *ptrMax, ProgressListener *listener) {
+  return patchDict->load(ptr, ptrMax, listener);
+}
+
+void DictionaryManager::import(Dictionary *other, ProgressListener *listener) {}
+
+IteratorUCharString *DictionaryManager::getSubjects() {
+  return new DictManagerIterator(hdtDict->getSubjects(), patchDict->getSubjects());
+}
+
+IteratorUCharString *DictionaryManager::getPredicates() {
+  return new DictManagerIterator(hdtDict->getPredicates(), patchDict->getPredicates());
+}
+
+IteratorUCharString *DictionaryManager::getObjects() {
+  return new DictManagerIterator(hdtDict->getObjects(), patchDict->getObjects());
+}
+
+IteratorUCharString *DictionaryManager::getShared() {
+  return new DictManagerIterator(hdtDict->getShared(), patchDict->getShared());
+}
+
+void DictionaryManager::startProcessing(ProgressListener *listener) {}
+void DictionaryManager::stopProcessing(ProgressListener *listener) {}
+
+string DictionaryManager::getType() {
+    return HDTVocabulary::HDT_DICTIONARY_BASE+"Manager>";
+}
+
+unsigned int DictionaryManager::getMapping() {
+  return hdtDict->getMapping() + patchDict->getMapping();
+}
+
+void DictionaryManager::getSuggestions(const char *base, TripleComponentRole role, std::vector<string> &out, int maxResults) {}
--- src/main/cpp/dictionary/dictionary_manager.h
+++ src/main/cpp/dictionary/dictionary_manager.h
@@ -5,7 +5,7 @@
 using namespace std;
 using namespace hdt;
 
-class DictionaryManager {
+class DictionaryManager : public ModifiableDictionary {
 
   Dictionary *hdtDict;             // Dictionary from HDT file
   ModifiableDictionary *patchDict; // Additional dictionary
@@ -13,6 +13,7 @@ class DictionaryManager {
   const unsigned int bitmask;
 
 public:
+  DictionaryManager(Dictionary *hdtDict, ModifiableDictionary *patchDict);
   DictionaryManager(Dictionary *hdtDict);
   DictionaryManager();
   ~DictionaryManager();
@@ -45,4 +46,62 @@ public:
   *
   **/
   unsigned int insert(std::string &str, TripleComponentRole position);
+
+  /**
+  * Proxied methods
+  *
+  **/
+
+  size_t getNumberOfElements();
+  uint64_t size();
+
+  unsigned int getNsubjects();
+  unsigned int getNpredicates();
+  unsigned int getNobjects();
+  unsigned int getNshared();
+
+  unsigned int getMaxID();
+  unsigned int getMaxSubjectID();
+  unsigned int getMaxPredicateID();
+  unsigned int getMaxObjectID();
+
+  void populateHeader(Header &header, string rootNode);
+  void save(std::ostream &output, ControlInformation &ci, ProgressListener *listener = NULL);
+  void load(std::istream &input, ControlInformation &ci, ProgressListener *listener = NULL);
+
+  size_t load(unsigned char *ptr, unsigned char *ptrMax, ProgressListener *listener=NULL);
+
+  void import(Dictionary *other, ProgressListener *listener=NULL);
+
+  IteratorUCharString *getSubjects();
+  IteratorUCharString *getPredicates();
+  IteratorUCharString *getObjects();
+  IteratorUCharString *getShared();
+
+  void startProcessing(ProgressListener *listener = NULL);
+  void stopProcessing(ProgressListener *listener = NULL);
+
+  string getType();
+  unsigned int getMapping();
+
+  void getSuggestions(const char *base, TripleComponentRole role, std::vector<string> &out, int maxResults);
+};
+
+class DictManagerIterator : public IteratorUCharString {
+private:
+  IteratorUCharString * first;
+  IteratorUCharString * second;
+public:
+  DictManagerIterator(IteratorUCharString *first, IteratorUCharString *second) : first(first), second(second){
+
+  }
+  virtual ~DictManagerIterator() { }
+
+  virtual bool hasNext() {
+    return first->hasNext() || first->hasNext();
+  }
+
+  virtual unsigned char *next() {
+    return first->hasNext() ? first->next() : second->next();
+  }
 };
