705df48490a615850e09e3f67080b31f47877ab6
Ruben Taelman <ruben.taelman@ugent.be>
2016-09-08 10:49:17 +0200

    Correctly skip creation of existing patches in evaluator

--- src/main/cpp/controller/controller.cc
+++ src/main/cpp/controller/controller.cc
@@ -122,6 +122,7 @@ DictionaryManager *Controller::get_dictionary_manager(int patch_id) const {
 }
 
 int Controller::get_max_patch_id() {
+    get_snapshot_manager()->get_snapshot(0); // Make sure our first snapshot is loaded, otherwise KC might get intro trouble while reorganising since it needs the dict for that.
     int max_patch_id = get_patch_tree_manager()->get_max_patch_id(get_snapshot_manager()->get_dictionary_manager(0));
     if (max_patch_id < 0) {
         return get_snapshot_manager()->get_latest_snapshot(0);
