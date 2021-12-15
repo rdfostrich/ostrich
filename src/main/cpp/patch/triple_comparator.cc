7c12194ae08a8d53d0f1f3746d08cb0dd14d6e5f
Olivier Pelgrin <olivier@cs.aau.dk>
2021-12-15 16:17:50 +0100

    finish implementation of generic triple comparator

--- src/main/cpp/patch/triple_comparator.cc
+++ src/main/cpp/patch/triple_comparator.cc
@@ -1,5 +1,52 @@
 #include "triple_comparator.h"
 
+constexpr size_t bitmask = 1ULL << (8 * sizeof(size_t) - 1);
+
+
+triplecomp subject_comparator = [] (const Triple& t1, const Triple& t2, std::shared_ptr<DictionaryManager> dict1, std::shared_ptr<DictionaryManager> dict2) {
+    size_t max_id = (size_t) -1;
+    if (t1.get_subject() == max_id || t2.get_subject() == 0) return 1;
+    if (t2.get_subject() == max_id || t1.get_subject() == 0) return -1;
+    // the triples are using the same dictionary
+    if (dict1 == dict2) {
+        // If MSB is not set, id is HDT
+        if (!(t1.get_subject() & bitmask) && !(t2.get_subject() & bitmask)) {
+            return dict1->compareComponent(t1.get_subject(), t2.get_subject(), SUBJECT);
+        }
+    }
+    //Else, translate to string and compare
+    return t1.get_subject(*dict1).compare(t2.get_subject(*dict2));
+};
+
+triplecomp predicate_comparator = [] (const Triple& t1, const Triple& t2, std::shared_ptr<DictionaryManager> dict1, std::shared_ptr<DictionaryManager> dict2) {
+    size_t max_id = (size_t) -1;
+    if (t1.get_predicate() == max_id || t2.get_predicate() == 0) return 1;
+    if (t2.get_predicate() == max_id || t1.get_predicate() == 0) return -1;
+    if (dict1 == dict2) {
+        // If MSB is not set, id is HDT
+        if (!(t1.get_predicate() & bitmask) && !(t2.get_predicate() & bitmask)) {
+            return dict1->compareComponent(t1.get_predicate(), t2.get_predicate(), PREDICATE);
+        }
+    }
+    //Else, translate to string and compare
+    return t1.get_predicate(*dict1).compare(t2.get_predicate(*dict2));
+};
+
+triplecomp object_comparator = [] (const Triple& t1, const Triple& t2, std::shared_ptr<DictionaryManager> dict1, std::shared_ptr<DictionaryManager> dict2) {
+    size_t max_id = (size_t) -1;
+    if (t1.get_object() == max_id || t2.get_object() == 0) return 1;
+    if (t2.get_object() == max_id || t1.get_object() == 0) return -1;
+    if (dict1 == dict2) {
+        // If MSB is not set, id is HDT
+        if (!(t1.get_object() & bitmask) && !(t2.get_object() & bitmask)) {
+            return dict1->compareComponent(t1.get_object(), t2.get_object(), OBJECT);
+        }
+    }
+    //Else, translate to string and compare
+    return t1.get_object(*dict1).compare(t2.get_object(*dict2));
+};
+
+
 TripleComparator::TripleComparator(triplecomp comp1, triplecomp comp2, triplecomp comp3, std::shared_ptr<DictionaryManager> dict1, std::shared_ptr<DictionaryManager> dict2): comp1(comp1), comp2(comp2), comp3(comp3), dict1(dict1), dict2(dict2) {}
 
 int TripleComparator::compare(const Triple &triple1, const Triple &triple2) const {
@@ -25,3 +72,23 @@ void TripleComparator::set_dictionary2(std::shared_ptr<DictionaryManager> dict) 
     dict2 = std::move(dict);
 }
 
+
+
+TripleComparator get_triple_comparator(TripleComponentOrder order, std::shared_ptr<DictionaryManager> dict1, std::shared_ptr<DictionaryManager> dict2) {
+    switch (order) {
+        case SPO:
+            return {subject_comparator, predicate_comparator, object_comparator, dict1, dict2};
+        case SOP:
+            return {subject_comparator, object_comparator, predicate_comparator, dict1, dict2};
+        case PSO:
+            return {predicate_comparator, subject_comparator, object_comparator, dict1, dict2};
+        case POS:
+            return {predicate_comparator, object_comparator, subject_comparator, dict1, dict2};
+        case OSP:
+            return {object_comparator, subject_comparator, predicate_comparator, dict1, dict2};
+        case OPS:
+            return {object_comparator, predicate_comparator, subject_comparator, dict1, dict2};
+        case Unknown:
+            return {subject_comparator, predicate_comparator, object_comparator, dict1, dict2};
+    }
+}
--- src/main/cpp/patch/triple_comparator.h
+++ src/main/cpp/patch/triple_comparator.h
@@ -6,53 +6,13 @@
 #include "../dictionary/dictionary_manager.h"
 #include "triple.h"
 
-constexpr size_t bitmask = 1ULL << (8 * sizeof(size_t) - 1);
-
 
 typedef std::function<int32_t(const Triple& t1, const Triple& t2, std::shared_ptr<DictionaryManager> dict1, std::shared_ptr<DictionaryManager> dict2)> triplecomp;
 
-triplecomp subject_comparator = [] (const Triple& t1, const Triple& t2, std::shared_ptr<DictionaryManager> dict1, std::shared_ptr<DictionaryManager> dict2) {
-    size_t max_id = (size_t) -1;
-    if (t1.get_subject() == max_id || t2.get_subject() == 0) return 1;
-    if (t2.get_subject() == max_id || t1.get_subject() == 0) return -1;
-    // the triples are using the same dictionary
-    if (dict1 == dict2) {
-        // If MSB is not set, id is HDT
-        if (!(t1.get_subject() & bitmask) && !(t2.get_subject() & bitmask)) {
-            return dict1->compareComponent(t1.get_subject(), t2.get_subject(), SUBJECT);
-        }
-    }
-    //Else, translate to string and compare
-    return t1.get_subject(*dict1).compare(t2.get_subject(*dict2));
-};
+extern triplecomp subject_comparator;
+extern triplecomp predicate_comparator;
+extern triplecomp object_comparator;
 
-triplecomp predicate_comparator = [] (const Triple& t1, const Triple& t2, std::shared_ptr<DictionaryManager> dict1, std::shared_ptr<DictionaryManager> dict2) {
-    size_t max_id = (size_t) -1;
-    if (t1.get_predicate() == max_id || t2.get_predicate() == 0) return 1;
-    if (t2.get_predicate() == max_id || t1.get_predicate() == 0) return -1;
-    if (dict1 == dict2) {
-        // If MSB is not set, id is HDT
-        if (!(t1.get_predicate() & bitmask) && !(t2.get_predicate() & bitmask)) {
-            return dict1->compareComponent(t1.get_predicate(), t2.get_predicate(), PREDICATE);
-        }
-    }
-    //Else, translate to string and compare
-    return t1.get_predicate(*dict1).compare(t2.get_predicate(*dict2));
-};
-
-triplecomp object_comparator = [] (const Triple& t1, const Triple& t2, std::shared_ptr<DictionaryManager> dict1, std::shared_ptr<DictionaryManager> dict2) {
-    size_t max_id = (size_t) -1;
-    if (t1.get_object() == max_id || t2.get_object() == 0) return 1;
-    if (t2.get_object() == max_id || t1.get_object() == 0) return -1;
-    if (dict1 == dict2) {
-        // If MSB is not set, id is HDT
-        if (!(t1.get_object() & bitmask) && !(t2.get_object() & bitmask)) {
-            return dict1->compareComponent(t1.get_object(), t2.get_object(), OBJECT);
-        }
-    }
-    //Else, translate to string and compare
-    return t1.get_object(*dict1).compare(t2.get_object(*dict2));
-};
 
 class TripleComparator {
 private:
@@ -70,6 +30,9 @@ public:
     void set_dictionary1(std::shared_ptr<DictionaryManager> dict);
     void set_dictionary2(std::shared_ptr<DictionaryManager> dict);
 };
+
+
+TripleComparator get_triple_comparator(TripleComponentOrder order);
 
 
 #endif //OSTRICH_TRIPLE_COMPARATOR_H
