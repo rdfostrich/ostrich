e662804ad96726ff4dda9fdf3f0092d429100666
Olivier Pelgrin <olivier@cs.aau.dk>
2022-09-01 15:33:48 +0200

    first implementation of the new compressed patch tree addition value

--- src/main/cpp/patch/patch_tree_addition_value.cc
+++ src/main/cpp/patch/patch_tree_addition_value.cc
@@ -1,10 +1,12 @@
 #include <cstring>
 #include <string>
 #include <cstdlib>
+#include <limits>
 #include "patch_tree_addition_value.h"
 
-PatchTreeAdditionValue::PatchTreeAdditionValue() : patches(), local_changes() {}
 
+#ifndef COMPRESSED_TREE_VALUES
+PatchTreeAdditionValue::PatchTreeAdditionValue() : patches(), local_changes() {}
 
 void PatchTreeAdditionValue::add(int patch_id) {
     std::vector<int>::iterator itToInsert = std::lower_bound(
@@ -41,7 +43,6 @@ void PatchTreeAdditionValue::set_local_change(int patch_id) {
     if(itToInsert == local_changes.end() || *itToInsert != patch_id) {
         local_changes.insert(itToInsert, patch_id);
     }
-
 }
 
 bool PatchTreeAdditionValue::is_local_change(int patch_id) const {
@@ -68,7 +69,7 @@ const char *PatchTreeAdditionValue::serialize(size_t *size) const {
     size_t local_changes_size = local_changes.size();
     size_t size_t_size_bits = sizeof(size_t);
     size_t patches_size_bits = patches_size * sizeof(int);
-    size_t local_changes_size_bits = local_changes_size * sizeof(bool);
+    size_t local_changes_size_bits = local_changes_size * sizeof(int);
 
     *size = size_t_size_bits + patches_size_bits + local_changes_size_bits;
     char* bytes = (char *) malloc(*size);
@@ -83,7 +84,7 @@ const char *PatchTreeAdditionValue::serialize(size_t *size) const {
 
     // Append local changes
     for(int i = 0; i < local_changes.size(); i++) {
-        std::memcpy(&bytes[size_t_size_bits + patches_size_bits + i * sizeof(bool)], &local_changes[i], sizeof(bool));
+        std::memcpy(&bytes[size_t_size_bits + patches_size_bits + i * sizeof(int)], &local_changes[i], sizeof(int));
     }
     return bytes;
 }
@@ -94,18 +95,115 @@ void PatchTreeAdditionValue::deserialize(const char *data, size_t size) {
     std::memcpy(&patches_size, data, sizeof(size_t));
     size_t size_t_size_bits = sizeof(size_t);
     // Calculate local changes size based on remaining bits
-    local_changes_size = (size - size_t_size_bits - (patches_size * sizeof(int))) / sizeof(bool);
+    local_changes_size = (size - size_t_size_bits - (patches_size * sizeof(int))) / sizeof(int);
     size_t patches_size_bits = patches_size * sizeof(int);
 
     // Read patches
     patches.resize(patches_size);
     for(int i = 0; i < patches_size; i++) {
-        std::memcpy(&patches.data()[i], &data[size_t_size_bits + i * sizeof(int)], sizeof(int));
+        std::memcpy(&patches[i], &data[size_t_size_bits + i * sizeof(int)], sizeof(int));
     }
 
     // Read local changes
     local_changes.resize(local_changes_size);
     for(int i = 0; i < local_changes_size; i++) {
-        std::memcpy(&local_changes.data()[i], &data[size_t_size_bits + patches_size_bits + i * sizeof(bool)], sizeof(bool));
+        std::memcpy(&local_changes[i], &data[size_t_size_bits + patches_size_bits + i * sizeof(int)], sizeof(int));
     }
 }
+
+#else
+
+PatchTreeAdditionValue::PatchTreeAdditionValue(int max_patch_id) : patches(std::numeric_limits<int>::max()),
+                                                   local_changes(std::numeric_limits<int>::max()),
+                                                   max_patch_id(max_patch_id) {}
+
+bool PatchTreeAdditionValue::add(int patch_id) {
+    return patches.addition(patch_id);
+}
+
+bool PatchTreeAdditionValue::del(int patch_id) {
+    return patches.deletion(patch_id);
+}
+
+bool PatchTreeAdditionValue::is_patch_id(int patch_id) const {
+    return patches.is_in(patch_id);
+}
+
+int PatchTreeAdditionValue::get_patch_id_at(long i) const {
+    int index = 0;
+    for (auto inter: patches.get_internal_representation()) {
+        int max_value = inter.second == patches.get_max_value() ? max_patch_id : inter.second;
+        int range = max_value - inter.first;
+        if (i < index + range) {
+            return inter.first + (i - index);
+        }
+        index += range;
+    }
+    return -1;
+}
+
+long PatchTreeAdditionValue::get_size() const {
+    int size = 0;
+    for (auto inter: patches.get_internal_representation()) {
+        int max_value = inter.second == patches.get_max_value() ? max_patch_id : inter.second;
+        int range = max_value - inter.first;
+        size += range;
+    }
+    return size;
+}
+
+bool PatchTreeAdditionValue::set_local_change(int patch_id) {
+    return local_changes.addition(patch_id);
+}
+
+bool PatchTreeAdditionValue::unset_local_change(int patch_id) {
+    return local_changes.deletion(patch_id);
+}
+
+bool PatchTreeAdditionValue::is_local_change(int patch_id) const {
+    return local_changes.is_in(patch_id);
+}
+
+std::string PatchTreeAdditionValue::to_string() const {
+    std::string ret = "{";
+    bool separator = false;
+    for(int i = 0; i < get_size(); i++) {
+        if(separator) ret += ",";
+        separator = true;
+        int p = get_patch_id_at(i);
+        ret += std::to_string(p) + (is_local_change(p) ? "L" : "");
+    }
+    ret += "}";
+    return ret;
+}
+
+const char *PatchTreeAdditionValue::serialize(size_t *size) const {
+    auto bin_patches = patches.serialize();
+    auto bin_local = local_changes.serialize();
+    *size = bin_patches.second + bin_local.second + sizeof(size_t);
+    char* data = new char[*size];
+    std::memcpy(data, &bin_patches.second, sizeof(size_t));
+    std::memcpy(data+sizeof(size_t), bin_patches.first, bin_patches.second);
+    if (bin_local.second > 0) {
+        std::memcpy(data+sizeof(size_t)+bin_patches.second, bin_local.first, bin_local.second);
+    }
+    delete[] bin_patches.first;
+    delete[] bin_local.first;
+    return data;
+}
+
+void PatchTreeAdditionValue::deserialize(const char *data, size_t size) {
+    size_t patches_size;
+    std::memcpy(&patches_size, data, sizeof(size_t));
+    size_t local_change_size = size - patches_size;
+    patches.deserialize(data+sizeof(size_t), patches_size);
+    if (local_change_size > 0) {
+        local_changes.deserialize(data+sizeof(size_t)+patches_size, local_change_size);
+    }
+    delete[] data;
+}
+
+
+#endif
+
+
--- src/main/cpp/patch/patch_tree_addition_value.h
+++ src/main/cpp/patch/patch_tree_addition_value.h
@@ -3,8 +3,12 @@
 
 #include <vector>
 #include <string>
-#include <stddef.h>
+#include <cstddef>
+#include "interval_list.h"
 
+#define COMPRESSED_TREE_VALUES
+
+#ifndef COMPRESSED_TREE_VALUES
 class PatchTreeAdditionValue {
 protected:
     std::vector<int> patches;
@@ -68,6 +72,73 @@ public:
      */
     void deserialize(const char* data, size_t size);
 };
-
+#else
+class PatchTreeAdditionValue {
+protected:
+    IntervalList<int> patches;
+    IntervalList<int> local_changes;
+    int max_patch_id;
+public:
+    explicit PatchTreeAdditionValue(int max_patch_id);
+    /**
+     * Add the given patch id.
+     * @param patch_id The patch id to add
+     * @return true if the internal representation changed (thus needing a disk rewrite)
+     */
+    bool add(int patch_id);
+    /**
+     * Mark the patch id as a deletion. Mark the end of an interval in the internal representation.
+     * @param patch_id the patch id to mark as deleted
+     * @return true if the internal representation changed (thus needing a disk rewrite)
+     */
+    bool del(int patch_id);
+    /**
+     * Check if the given patch id is present in this value.
+     * @param patch_id The id of the patch to find
+     * @return If the given patch id is present
+     */
+    bool is_patch_id(int patch_id) const;
+    /**
+     * Get a patch id by position.
+     * @param i The index to check the patch id for
+     * @return The patch id.
+     */
+    int get_patch_id_at(long i) const;
+    /**
+     * @return The number of PatchTreeDeletionValueElement's stored in this value.
+     */
+    long get_size() const;
+    /**
+     * Mark the given patch element as being a local change.
+     * @param patch_id The id of the patch to set
+     */
+    bool set_local_change(int patch_id);
+    bool unset_local_change(int patch_id);
+    /**
+     * Check if this element is an element (+) relative to the given patch itself,
+     * For example in the series [t1- t1+ t1-], the element at index 1 is a local change,
+     * while the others are global changes (with respect to the snapshot).
+     * @param patch_id The id of the patch to get
+     * @return If it is a local change.
+     */
+    bool is_local_change(int patch_id) const;
+    /**
+     * @return The string representation of this value.
+     */
+    std::string to_string() const;
+    /**
+     * Serialize this value to a byte array
+     * @param size This will contain the size of the returned byte array
+     * @return The byte array
+     */
+    const char* serialize(size_t* size) const;
+    /**
+     * Deserialize the given byte array to this object.
+     * @param data The data to deserialize from.
+     * @param size The size of the byte array
+     */
+    void deserialize(const char* data, size_t size);
+};
+#endif
 
 #endif //TPFPATCH_STORE_PATCH_TREE_ADDITION_VALUE_H
